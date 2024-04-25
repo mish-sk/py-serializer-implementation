be0eaa4b75391a1f1628ec8beac7d403dab07f0c
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2024-04-25 09:36:29 +0200

    Solution

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ def deserialize_car_object(json: bytes) -> Car:
     data = JSONParser().parse(stream)
     serializer = CarSerializer(data=data)
     if serializer.is_valid():
-        serializer.save()
-        return serializer.data
+        car_instance = serializer.save()
+        return car_instance
